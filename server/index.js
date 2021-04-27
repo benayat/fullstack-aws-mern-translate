fdfdb7df68290a51911910b6c21e1607d8cc4ac3
Benaya <benaya7@gmail.com>
2021-04-27 23:01:42 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require("./db/mongoose");
 const path = require("path");
 const port = process.env.PORT || 5000;
 const app = express();
+
 const recipeRouter = require("./routes/recipeRouter");
 
 if (process.env.NODE_ENV === "production") {
@@ -10,6 +11,7 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.use(express.static(path.join(__dirname, "../client/public")));
 }
+app.use("cors");
 app.use(express.json());
 app.use("/api/recipes", recipeRouter);
 
