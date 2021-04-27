4c2aadf9457da4d1124d6ce1ddf5116b30a20ed7
Itay <eizzo77@gmail.com>
2021-04-27 22:18:00 +0300

    some updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Header } from "./components/Header.components";
 import FindRecipesPage from "./pages/FindRecipesPage";
-// import FavoritesPage from "./components/FavoritesPage.components";
+import FavoritesPage from "./components/FavoritesPage";
 import { NotFoundPage } from "./pages/NotFoundPage.components";
 import "./App.css";
 
@@ -12,7 +12,7 @@ export const App = () => {
         <Header />
         <Switch>
           <Route path="/recipes" exact component={FindRecipesPage} />
-          {/* <Route path="/favorites" exact component={FavoritesPage} /> */}
+          <Route path="/myrecipes" exact component={FavoritesPage} />
           <Route path="/" component={NotFoundPage} />
         </Switch>
       </BrowserRouter>
