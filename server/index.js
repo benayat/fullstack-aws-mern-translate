8a55d1a500a8daeb17269af86c8eead30fdf94f1
Benaya <benaya7@gmail.com>
2021-04-27 21:46:12 +0300

    dkjvnd

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 require("./db/mongoose");
+const path = require(path);
 const port = process.env.PORT || 5000;
 const app = express();
 const recipeRouter = require("./routes/recipeRouter");
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+} else {
+  app.use(express.static(path.join(__dirname, "../client/public")));
+}
 app.use(express.json());
 app.use("/api/recipes", recipeRouter);
 
