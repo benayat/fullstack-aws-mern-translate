4e12df27a7884258b179219085a3f6c63ca72106
Benaya <benaya7@gmail.com>
2021-04-27 23:06:27 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.use(express.static(path.join(__dirname, "../client/public")));
 }
-app.use("cors");
+app.use(cors());
 app.use(express.json());
 app.use("/api/recipes", recipeRouter);
 
