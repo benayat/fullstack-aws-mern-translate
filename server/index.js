0232c97bb7a9d74e4754ffe5beec244b2d20aa5e
Benaya <benaya7@gmail.com>
2021-04-28 01:05:37 +0300

    skjnvd

--- server/index.js
+++ server/index.js
@@ -5,12 +5,8 @@ const path = require("path");
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(express.json());
 
