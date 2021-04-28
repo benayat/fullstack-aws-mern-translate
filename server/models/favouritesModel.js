d96de0e4412762b946d47c81316881139a22ebf7
Benaya <benaya7@gmail.com>
2021-04-28 04:21:11 +0300

    lsdkvmdlfkm

--- server/models/favouritesModel.js
+++ server/models/favouritesModel.js
@@ -1,3 +1,4 @@
 const mongoose = require("mongoose");
 const FavouriteSchema = require("mongoose").model("recipe").schema;
-export const Favourite = mongoose.model("favourite", FavouriteSchema);
+const Favourite = mongoose.model("favourite", FavouriteSchema);
+module.exports = Favourite;
